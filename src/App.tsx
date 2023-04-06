ac3b0ca6d245591d957015c732e6fb71a8e6c6ce
Ghost <alirazamunir401@gmail.com>
2023-04-06 06:35:06 +0500

    add FB

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,10 @@ const imagesArray = [
   "https://images4.alphacoders.com/560/560170.jpg",
   "https://c4.wallpaperflare.com/wallpaper/997/210/533/anime-attack-on-titan-attack-on-titan-levi-ackerman-wallpaper-preview.jpg",
   "https://c4.wallpaperflare.com/wallpaper/444/553/202/digital-art-artwork-anime-anime-boys-wallpaper-preview.jpg",
-  // "https://images8.alphacoders.com/505/505616.png",
-  // "https://images6.alphacoders.com/451/451608.jpg",
-  // "https://images3.alphacoders.com/848/848449.png",
-  // "https://images4.alphacoders.com/560/560170.jpg",
-  // "https://c4.wallpaperflare.com/wallpaper/997/210/533/anime-attack-on-titan-attack-on-titan-levi-ackerman-wallpaper-preview.jpg",
-  // "https://c4.wallpaperflare.com/wallpaper/444/553/202/digital-art-artwork-anime-anime-boys-wallpaper-preview.jpg",
 ];
 
 const StyledImage = styled.img`
   object-fit: cover;
-  /* height: 600px; */
   width: 100%;
   height: 100%;
   border-radius: 8px;
